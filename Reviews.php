ce66a06565782d7ffb3f21fb3f6edb82b504c6a7
jonipablo <jonirpablo@gmail.com>
2016-09-24 23:39:20 +1200

    Added Reviews.php

--- Reviews.php
+++ Reviews.php
@@ -11,10 +11,10 @@ and open the template in the editor.
         <link rel="stylesheet" type="text/css" href="Styles/MainStyleSheet.css"/>
     </head>
     <body>
-        
+
         <nav class="navigation">
             <ul id="nav">
-                  <a href="Home.php"><img src="Images/Logo/GBLogosmall.png" width="232" height="50" id="logo" /></a> 
+                <a href="Home.php"><img src="Images/Logo/GBLogosmall.png" width="232" height="50" id="logo" /></a> 
                 <li><a href="Home.php">HOME</a></li>
                 <li class="dropdown" style="position: relative; top:">
                     <div><a href="javascript:void(0)" class="dropbtn" onclick="myFunction()">PLATFORM</a></div>
@@ -28,40 +28,42 @@ and open the template in the editor.
                 <li><a href="Login.php#">LOGIN</a></li>
             </ul>
         </nav>
-       
-        
-        
+
+
+
         <div id="banner">
         </div>
-        
+
         <div id="wrapper">
-            
+
             <div class="columns">
                 <div id="review_col1">
                     <h1>Review Here!</h1>
                 </div>
             </div>
-            
-            
+
+
             <div id="sidebar">
-                <a href="Top10.php"><b><br>TOP 10 GAMES<body link="#FFFFFF" vlink="#FFFFFF"></b><br><br></a>
-                <a href="Articles/Article(Warhammer).php"><img src="Images/Sidebar/warhammer_banner.png" alt="warhammer" width="300" height="60" border="0"></a>
-                <a href="Articles/Article(QuadrilateralCowboy).php"><img src="Images/Sidebar/quad_banner.png" alt="quadrilateral" width="300" height="60" border="0"></a>
-                <a href="Articles/Article(Doom).php"><img src="Images/Sidebar/doom_banner.png" alt="doom" width="300" height="60" border="0"></a>
-                <a href="Articles/Article(DarkSoulsIII).php"><img src="Images/Sidebar/dsouls_banner.png" alt="darksouls" width="300" height="60" border="0"></a>
-                <a href="Articles/Article(Inside).php"><img src="Images/Sidebar/inside_banner.png" alt="inside" width="300" height="60" border="0"></a>
-                <a href="Articles/Article(Overwatch).php"><img src="Images/Sidebar/overwatch_banner.png" alt="overwatch" width="300" height="60" border="0"></a>
-                <a href="Articles/Article(HeartsOfIronIV).php"><img src="Images/Sidebar/heartsofiron_banner.png" alt="heartsofironiv" width="300" height="60" border="0"></a>
-                <a href="Articles/Article(Uncharted4).php"><img src="Images/Sidebar/uncharted4_banner.png" alt="uncharted4" width="300" height="60" border="0"></a>
-                <a href="Articles/Article(Dota2).php"><img src="Images/Sidebar/dota2_banner.png" alt="dota2" width="300" height="60" border="0"></a>
-                <a href="Articles/Article(JustCause3).php"><img src="Images/Sidebar/justcause_banner.png" alt="justcause3" width="300" height="60" border="0"></a>
+                <div id="sidebar">
+                    <a href="Top10.php"><b><br>TOP 10 GAMES</b><br><br></a>
+                    <a href="Articles/Article(Warhammer).php"><img src="Images/Sidebar/warhammer_banner.png" alt="warhammer" width="340" height="70" border="0"></a>
+                    <a href="Articles/Article(QuadrilateralCowboy).php"><img src="Images/Sidebar/quad_banner.png" alt="quadrilateral" width="340" height="70" border="0"></a>
+                    <a href="Articles/Article(Doom).php"><img src="Images/Sidebar/doom_banner.png" alt="doom" width="340" height="70" border="0"></a>
+                    <a href="Articles/Article(DarkSoulsIII).php"><img src="Images/Sidebar/dsouls_banner.png" alt="darksouls" width="340" height="70" border="0"></a>
+                    <a href="Articles/Article(Inside).php"><img src="Images/Sidebar/inside_banner.png" alt="inside" width="340" height="70" border="0"></a>
+                    <a href="Articles/Article(Overwatch).php"><img src="Images/Sidebar/overwatch_banner.png" alt="overwatch" width="340" height="70" border="0"></a>
+                    <a href="Articles/Article(HeartsOfIronIV).php"><img src="Images/Sidebar/heartsofiron_banner.png" alt="heartsofironiv" width="340" height="70" border="0"></a>
+                    <a href="Articles/Article(Uncharted4).php"><img src="Images/Sidebar/uncharted4_banner.png" alt="uncharted4" width="340" height="70" border="0"></a>
+                    <a href="Articles/Article(Dota2).php"><img src="Images/Sidebar/dota2_banner.png" alt="dota2" width="340" height="70" border="0"></a>
+                    <a href="Articles/Article(JustCause3).php"><img src="Images/Sidebar/justcause_banner.png" alt="justcause3" width="340" height="70" border="0"></a>
+                </div>
             </div>
-            
-            
+
+
         </div>
-        
-        
-        
-        
+
+
+
+
     </body>
 </html>
