8eb4244f3a321a4648f5e900a8d935e2567f33de
jonipablo <jonirpablo@gmail.com>
2016-10-14 02:19:49 +1300

    Added missing files

---
+++ Games/Top10-Sidebar.php
@@ -0,0 +1,15 @@
+<!-- Sidebar Section - Top 10 Games (File to include to article pages to display the sidebar -->
+<div id="sidebar">
+    <a href="../Top10.php"><b><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TOP 10 GAMES</b><br><br></a>
+    <center>
+        <a href="../Games/Warhammer.php"><img src="../Images/Sidebar/warhammer_banner.png" alt="warhammer" width="300" height="60" border="0"></a>
+        <a href="../Games/QuadrilateralCowboy.php"><img src="../Images/Sidebar/quad_banner.png" alt="quadrilateral" width="300" height="60" border="0"></a>
+        <a href="../Games/Doom.php"><img src="../Images/Sidebar/doom_banner.png" alt="doom" width="300" height="60" border="0"></a>
+        <a href="../Games/DarkSouls3.php"><img src="../Images/Sidebar/dsouls_banner.png" alt="darksouls" width="300" height="60" border="0"></a>
+        <a href="../Games/Inside.php"><img src="../Images/Sidebar/inside_banner.png" alt="inside" width="300" height="60" border="0"></a>
+        <a href="../Games/Overwatch.php"><img src="../Images/Sidebar/overwatch_banner.png" alt="overwatch" width="300" height="60" border="0"></a>
+        <a href="../Games/HeartsOfIron4.php"><img src="../Images/Sidebar/heartsofiron_banner.png" alt="heartsofironiv" width="300" height="60" border="0"></a>
+        <a href="../Games/Uncharted4.php"><img src="../Images/Sidebar/uncharted4_banner.png" alt="uncharted4" width="300" height="60" border="0"></a>
+        <a href="../Games/Dota2.php"><img src="../Images/Sidebar/dota2_banner.png" alt="dota2" width="300" height="60" border="0"></a>
+        <a href="../Games/JustCause3.php"><img src="../Images/Sidebar/justcause_banner.png" alt="justcause3" width="300" height="60" border="0"></a>
+</div>
