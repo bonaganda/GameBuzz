4413e06e1c1aa8ad11b7fe4d4cd7fcb918d3b6e5
kimbcapati <kimbcapati@yahoo.com>
2016-10-17 20:03:47 +1300

    added comments

--- About.php
+++ About.php
@@ -28,28 +28,28 @@ and open the template in the editor.
 
                 <!-- This section contains information about our website and about us -->
                 <center>
-                <a style="color: red;"><h1>About Us</h1></a>
-                <img src="Images/Kim/aboutus.png" style="width: 600px; height: 260px;"> 
-                </br></br>
-                Gamebuzz is a universal gaming community with a solid focus on popular competitive games. We have created this website to offer our users the latest news on newly released games for different platforms and as well as game reviews, recommendations, tips and tricks.
-                </br></br></br>
-                <a style="color: red;"><h2>Meet the Creators!</h2></a>
-                </br>
-                <img src="Images/Kim/benjo1.png" width="100" height="120" align="left" hspace = "30">
-                </br></br>
-                <p align="left">Hi, my name is Benjo! (Product Owner)</br> I'm responsible for defining Stories and prioritizing the Team Backlog so as to streamline the execution of program priorities.</p>
-                </br></br></br></br>
-                <img src="Images/Kim/bona1.png" width="100" height="120" align="left" hspace = "30">
-                </br></br>
-                <p align="left">Hi, my name is Kim! (Scrum Master)</br> I'm the Scrum Master for this project and I make sure that the team adheres to Scrum's principles.</p>
-                </br></br></br></br>
-                <img src="Images/Kim/kim1.png" width="100" height="120" align="left" hspace = "30">
-                </br></br>
-                <p align="left">Hi, my name is Joni! (Website Designer) </br> I'm in charge of the design part for our website.</p>
-                </br></br></br></br>
-                <img src="Images/Kim/joni1.png" width="100" height="120" align="left" hspace = "30">
-                </br></br>
-                <p align="left">Hi, my name is Bona! (Team Coordinator) </br> I'm in charge of overseeing the processes and details of how our team reaches our goals.</p>
+                    <a style="color: red;"><h1>About Us</h1></a>
+                    <img src="Images/Kim/aboutus.png" style="width: 600px; height: 260px;"> 
+                    </br></br>
+                    Gamebuzz is a universal gaming community with a solid focus on popular competitive games. We have created this website to offer our users the latest news on newly released games for different platforms and as well as game reviews, recommendations, tips and tricks.
+                    </br></br></br>
+                    <a style="color: red;"><h2>Meet the Creators!</h2></a>
+                    </br>
+                    <img src="Images/Kim/benjo1.png" width="100" height="120" align="left" hspace = "30">
+                    </br></br>
+                    <p align="left">Hi, my name is Benjo! (Product Owner)</br> I'm responsible for defining Stories and prioritizing the Team Backlog so as to streamline the execution of program priorities.</p>
+                    </br></br></br></br>
+                    <img src="Images/Kim/bona1.png" width="100" height="120" align="left" hspace = "30">
+                    </br></br>
+                    <p align="left">Hi, my name is Kim! (Scrum Master)</br> I'm the Scrum Master for this project and I make sure that the team adheres to Scrum's principles.</p>
+                    </br></br></br></br>
+                    <img src="Images/Kim/kim1.png" width="100" height="120" align="left" hspace = "30">
+                    </br></br>
+                    <p align="left">Hi, my name is Joni! (Website Designer) </br> I'm in charge of the design part for our website.</p>
+                    </br></br></br></br>
+                    <img src="Images/Kim/joni1.png" width="100" height="120" align="left" hspace = "30">
+                    </br></br>
+                    <p align="left">Hi, my name is Bona! (Team Coordinator) </br> I'm in charge of overseeing the processes and details of how our team reaches our goals.</p>
                 </center>
             </div>
         </div>
